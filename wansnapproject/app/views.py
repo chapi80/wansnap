d0a0cd58784079d4e0fa84b41f3fffa6eee2c9d8
“chapi80” <ostrmgn2@gmail.com>
2025-05-13 20:44:23 +0900

    app/views.pyのlogin_views追加

--- wansnapproject/app/views.py
+++ wansnapproject/app/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 def index(request):
     return render(request, 'portfolio/index.html')
 
-def login_view(reqest):
-    return render(reqest, 'app/login/.html')
+def login_view(request):
+    if request.meshod == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(redirect, 'メールアドレスまたはパスワードが間違っています')
+    
+    return render(request, 'app/login.html')
