74a2d7054d675ed886e59f5c20daba7ce89d6e82
“chapi80” <ostrmgn2@gmail.com>
2025-05-17 21:00:55 +0900

    ログイン画面が表示できるよう修正

--- wansnapproject/app/views.py
+++ wansnapproject/app/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib import messages
+from django.shortcuts import render, redirect
 
 def index(request):
     return render(request, 'portfolio/index.html')
 
 def login_view(request):
-    if request.meshod == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
         
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
             return redirect('home')
         else:
-            messages.error(redirect, 'メールアドレスまたはパスワードが間違っています')
+            return render(redirect, 'app/login.html', {'error': 'メールアドレスまたはパスワードが間違っています'})
     
     return render(request, 'app/login.html')
