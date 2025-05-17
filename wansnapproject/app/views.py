56869c09d95ba9bcd70820a65b0253e35f633ce5
“chapi80” <ostrmgn2@gmail.com>
2025-05-18 08:53:42 +0900

    forms.pyの内容をlogin.htmlに反映

--- wansnapproject/app/views.py
+++ wansnapproject/app/views.py
@@ -7,17 +7,18 @@ def index(request):
     return render(request, 'portfolio/index.html')
 
 def login_view(request):
+    form = LoginForm(request.POST or None)
+    
     if request.method == 'POST':
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            form.add_error(None, 'メールアドレスまたはパスワードが間違っています')
-    else:
-        form = LoginForm()
-        
+        if form.is_valid():
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(request, username=email, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                form.add_error(None, 'メールアドレスまたはパスワードが間違っています')
+       
     return render(request, 'app/login.html', {'form':form})
