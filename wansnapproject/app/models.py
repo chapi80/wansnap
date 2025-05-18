a73e3dd9e32d401e9569a9173bd05173a471b537
“chapi80” <ostrmgn2@gmail.com>
2025-05-18 21:29:22 +0900

    models.pyにclass Dog追加

--- wansnapproject/app/models.py
+++ wansnapproject/app/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import(
 )
 
 class User(AbstractBaseUser, PermissionsMixin):
-    usermane = models.CharField(max_length=50)
+    username = models.CharField(max_length=50)
     email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
@@ -14,3 +14,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     class Meta:
         db_table = 'user'
+        
+class Dog(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='dogs')
+    name = models.CharField(max_length=50)
+    breed = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=[('男の子','男の子'), ('女の子','女の子')])
+    birthday = models.DateField(null=True, blank=True)
+    dog_image = models.ImageField(upload_to='dog_image/', null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.name}({self.owner.username}のうちの子)"
