8ddc7f4b16f3fa2574ff0ca2d6dd1ac9cd4d4e65
“chapi80” <ostrmgn2@gmail.com>
2025-05-18 09:17:07 +0900

    新規アカウント作成forms.py追加

--- wansnapproject/app/forms.py
+++ wansnapproject/app/forms.py
@@ -1,6 +1,14 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label='メールアドレス', required=True)
     password = forms.CharField(label='パスワード', widget=forms.PasswordInput, required=True)
+
+class SignuoForm(forms.Modelform):
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput)
+    password_confirm = cleaned_date.get("password_confirm")
     
+    if password and password_confirm and password != password_confirm:
+        self.add_error('password_confirm', "パスワードが一致しません")
+        
