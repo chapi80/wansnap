3446b424c3fd877bdde6b02b8c07d4fd9473ab4e
“chapi80” <ostrmgn2@gmail.com>
2025-05-18 20:56:00 +0900

    forms.pyのclass SignupForm修正

--- wansnapproject/app/forms.py
+++ wansnapproject/app/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label='メールアドレス', required=True)
     password = forms.CharField(label='パスワード', widget=forms.PasswordInput, required=True)
 
-class SignupForm(forms.Modelform):
-    password = forms.CharField(label='パスワード', widget=forms.PasswordInput)
-    password_confirm = cleaned_date.get("password_confirm")
+class SignupForm(forms.ModelForm):
+    password1 = forms.CharField(label='パスワード', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='パスワード（再入力）', widget=forms.PasswordInput)
     
-    if password and password_confirm and password != password_confirm:
-        self.add_error('password_confirm', "パスワードが一致しません")
+    class Meta:
+        model =User
+        filrds = ['username', 'email']
         
+        def clean(self):
+            cleaned_date = super().clean()
+            password1 = cleaned_date.get("password1")
+            password2 = cleaned_date.get("password2")
+            
+            if password1 and password2 and password1 != password2:
+                self.add_error('password2', "パスワードが一致しません")
