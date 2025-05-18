212613f4e4814526dc4a23dd9db192488842fb76
“chapi80” <ostrmgn2@gmail.com>
2025-05-18 09:26:41 +0900

    新規アカウント作成views.py追加

--- wansnapproject/app/views.py
+++ wansnapproject/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.models import User
+from .forms import SignupForm
 
 
 def index(request):
@@ -22,3 +24,18 @@ def login_view(request):
                 form.add_error(None, 'メールアドレスまたはパスワードが間違っています')
        
     return render(request, 'app/login.html', {'form':form})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            password = form.cleaned_date['password']
+            user.set_password(password)
+            user.save()
+            login(request.user)
+            return redirect('home')
+        else:
+            form =SignupForm()
+        
+        return render(request, 'app/signup.html', {'form':form})
