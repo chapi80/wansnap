63657a4323bc7930e91bb2dbdb865ce70ab82e27
“chapi80” <ostrmgn2@gmail.com>
2025-05-18 09:08:23 +0900

    スペルミス修正

--- wansnapproject/app/forms.py
+++ wansnapproject/app/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 class LoginForm(forms.Form):
     email = forms.EmailField(label='メールアドレス', required=True)
-    password = forms.CharFild(label='パスワード', widget=forms.PasswordInput, required=True)
+    password = forms.CharField(label='パスワード', widget=forms.PasswordInput, required=True)
     
