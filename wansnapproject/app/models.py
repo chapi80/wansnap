f411c268edac82478186145d026d20ec2082c537
“chapi80” <ostrmgn2@gmail.com>
2025-05-22 22:55:08 +0900

    管理画面用コード追加

--- wansnapproject/app/models.py
+++ wansnapproject/app/models.py
@@ -2,12 +2,31 @@ from django.db import models
 from django.contrib.auth.models import(
     AbstractBaseUser, PermissionsMixin
 )
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('メールアドレスは必須です')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(self, email, username, password=None, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=50)
     email = models.EmailField(max_length=255, unique=True)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
