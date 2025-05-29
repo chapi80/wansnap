1f0b23845313ac173109bd0617a21dfc6e726048
“chapi80” <ostrmgn2@gmail.com>
2025-05-29 21:35:36 +0900

    プロジェクトのurls.pyとsettings.pyを修正

--- wansnapproject/wansnapproject/settings.py
+++ wansnapproject/wansnapproject/settings.py
@@ -131,8 +131,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL ='app.User'
 
