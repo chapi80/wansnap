a16116fca5b4b341c55d663379b52fbdec193ce6
“chapi80” <ostrmgn2@gmail.com>
2025-06-19 22:34:41 +0900

    URL修正

--- wansnapproject/wansnapproject/urls.py
+++ wansnapproject/wansnapproject/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
-    path('', include('app.urls')),
+    path('wansnap/', include('app.urls')),
+    path('', TemplateView.as_view(template_name='portfolio/index.html'), name='portfolio'),
 ]
 
 if settings.DEBUG:
