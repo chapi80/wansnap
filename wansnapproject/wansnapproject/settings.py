ce83f81c7abfb590a79070e1c209ab1c3d3418e3
“chapi80” <ostrmgn2@gmail.com>
2025-06-19 13:04:41 +0900

    settings.py修正

--- wansnapproject/wansnapproject/settings.py
+++ wansnapproject/wansnapproject/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -143,5 +146,3 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGOUT_REDIRECT_URL = 'login'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
